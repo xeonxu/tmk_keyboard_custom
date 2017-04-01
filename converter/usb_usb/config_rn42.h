3fe08ff71de3fed746659da3988e3922ebfef67e
Xeon Xu <xeonxu@gmail.com>
2017-04-01 14:04:41 +0800

    Define tapping term time.

--- converter/usb_usb/config_rn42.h
+++ converter/usb_usb/config_rn42.h
@@ -44,9 +44,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 // /* period of tapping(ms) */
-// #define TAPPING_TERM    300
+#define TAPPING_TERM    200
 // /* tap count needed for toggling a feature */
-// #define TAPPING_TOGGLE  5
+#define TAPPING_TOGGLE  5
 // /* Oneshot timeout(ms) */
 // #define ONESHOT_TIMEOUT 300
 
